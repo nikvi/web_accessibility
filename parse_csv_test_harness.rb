5bc0ebd959de27992eb190b1440c120f211954f7
Nikki Vinayan <nikki.vinayan@gmail.com>
2014-08-21 11:49:03 +1000

    Modified structure and removed

--- parse_csv_test_harness.rb
+++
@@ -1,9 +0,0 @@
-# Test Harness for ParseCSV Class
-require 'csv'
-require 'charlock_holmes/string'   
-require './ParseCSV' 
-
-myControlCSV = ParseCSV.new("Graduate Research - Page.csv","siteimprove")
-#myControlCSV = ParseCSV.new("Merged Sheet - Sheet1.csv","google")
-myControlCSV.get_titles
-myControlCSV.get_encoding
