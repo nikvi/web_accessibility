c9159f0b205d18c50486027934da7f60970b080c
unknown <vinayann@841L-92038-W.unimelb.edu.au>
2014-08-21 11:44:57 +1000

    Modified project structure

---
+++ test/parse_csv_test_harness.rb
@@ -0,0 +1,7 @@
+# Test Harness for ParseCSV Class
+require './test_helper.rb'
+
+myControlCSV = ParseCSV.new("../data_csv/Graduate Research - Page.csv","siteimprove")
+#myControlCSV = ParseCSV.new("../data/Merged Sheet - Sheet1.csv","google")
+myControlCSV.get_titles
+myControlCSV.get_encoding
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+#test_helper.rb
+# can be extendedd to provide data for later tests
+require 'csv'
+#remove comment to add charlock
+#require 'charlock_holmes/string'   
+require '../lib/parse_csv' 
