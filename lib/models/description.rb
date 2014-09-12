9fb46521e00be8760f05b8ec4e1c12b36fc56ca8
Andrew Normand <anormand@unimelb.edu.au>
2014-09-12 17:43:38 +1000

    Update description.rb
    Self inheritance turned off so that we can have a column called 'type'

--- lib/models/description.rb
+++ lib/models/description.rb
@@ -1,7 +1,8 @@
 #description.rb
 require 'sinatra'
 require 'sinatra/activerecord'
-
+require '../config/environments'
 
 class  Description < ActiveRecord::Base
+  self.inheritance_column = nil
 end
