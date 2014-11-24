d38acc4a9e2013a89a7f3b4509a7fcb6ef6d7fde
nikvi <nikki.vinayan@gmail.com>
2014-11-25 10:59:49 +1100

    modified environments.rb and added proc file for heroku deploy

--- config/environments.rb
+++ config/environments.rb
@@ -3,7 +3,17 @@ Sinatra::Base.configure :development, :production do
     enable :logging
     #dbconfig = YAML.load(File.read("config/database.yml")).with_indifferent_access
     # running test harness 
-    dbconfig = YAML.load(File.read("../config/database.yml")).with_indifferent_access
-    set :database, dbconfig[settings.environment]
-    ActiveRecord::Base.establish_connection dbconfig[settings.environment]
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ 
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+    #dbconfig = YAML.load(File.read("../config/database.yml")).with_indifferent_access
+    #set :database, dbconfig[settings.environment]
+    #ActiveRecord::Base.establish_connection dbconfig[settings.environment]
 end
